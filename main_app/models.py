f5a0ae84b4bf308a8e98de7d8c4def0501cb0fd2
Daniel JS <daniel@mydeveloperjourney.com>
2022-10-11 08:50:11 -0500

    wip: adds feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,4 @@
+from random import choices
 from django.db import models
 from django.urls import reverse
 
@@ -13,3 +14,17 @@ class Cat(models.Model):
     
     def get_absolute_url(self):
         return reverse('cats_detail', kwargs={'cat_id': self.id })
+
+class Feeding(models.Model):
+    MEALS = (
+        ('B', 'Breakfast'),
+        ('L', 'Lunch'),
+        ('D', 'Dinner'),
+    )
+
+    date = models.DateField('feeding date')
+    meal = models.CharField(max_length=1, choices=MEALS, default=MEALS[0][0])
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.get_meal_display()} on {self.date}'
