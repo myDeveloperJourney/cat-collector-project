cb3901bd3c8ca4e94c57d06fb6a3e0faa0e197d5
Daniel JS <daniel@mydeveloperjourney.com>
2022-10-13 09:03:21 -0500

    wip: adds login/logout features

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,6 @@
-from random import choices
-from statistics import mode
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Toy(models.Model):
@@ -20,6 +19,7 @@ class Cat(models.Model):
     description = models.TextField(max_length=250)
     age = models.IntegerField()
     toys = models.ManyToManyField(Toy)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
